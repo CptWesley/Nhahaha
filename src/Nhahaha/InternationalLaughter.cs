51edd99ee88e195013cf766a3a7f337400de2ffa
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2019-05-04 09:41:14 +0200

    Add international laughter

---
+++ src/Nhahaha/InternationalLaughter.cs
@@ -0,0 +1,12 @@
+﻿namespace Nhahaha
+{
+    /// <summary>
+    /// International laughter producing engine.
+    /// </summary>
+    public class InternationalLaughter : Laughter
+    {
+        /// <inheritdoc/>
+        public override string LaughOnce()
+            => "ha";
+    }
+}
