d35efe0e09cc7610c14e64d3149b2049c3f877b7
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2019-05-04 09:38:16 +0200

    Add Laughter abstract class

---
+++ src/Nhahaha.Tests/LaughterTests.cs
@@ -0,0 +1,141 @@
+﻿using System.Text;
+using Xunit;
+using static AssertNet.Xunit.Assertions;
+
+namespace Nhahaha.Tests
+{
+    /// <summary>
+    /// Test class for the <see cref="Laughter"/> class.
+    /// </summary>
+    public abstract class LaughterTests
+    {
+        private Laughter laughter;
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="LaughterTests"/> class.
+        /// </summary>
+        public LaughterTests()
+            => laughter = GetInstance();
+
+        /// <summary>
+        /// Gets an instance of <see cref="Laughter"/> under test.
+        /// </summary>
+        /// <returns>A <see cref="Laughter"/> instance for tetsing.</returns>
+        protected abstract Laughter GetInstance();
+
+        /// <summary>
+        /// Gets the expected single laugh string.
+        /// </summary>
+        /// <returns>The expected single laugh string.</returns>
+        protected abstract string GetExpectedOnce();
+
+        /// <summary>
+        /// Checks that the laugh once is the expected laugh.
+        /// </summary>
+        [Fact]
+        public void OnceTest()
+            => AssertThat(laughter.LaughOnce()).IsEqualTo(GetExpectedOnce());
+
+        /// <summary>
+        /// Checks that the default laughter is repeated a correct amount of times when capitalization is explicitly enabled.
+        /// </summary>
+        [Fact]
+        public void OnceCapitalizedTrueTest()
+        {
+            string once = laughter.LaughOnce(true);
+            AssertThat(char.IsUpper(once[0])).IsTrue();
+            AssertThat(once.Substring(1).ToCharArray()).AllSatisfy(c => char.IsLower(c));
+        }
+
+        /// <summary>
+        /// Checks that the laughter is repeated a correct amount of times when capitalization is explicitly disabled.
+        /// </summary>
+        [Fact]
+        public void OnceCapitalizeFalseTest()
+        {
+            string once = laughter.LaughOnce(false);
+            AssertThat(once.ToCharArray()).AllSatisfy(c => char.IsLower(c));
+        }
+
+        /// <summary>
+        /// Checks that the non-capitalized calls are strictly lower case.
+        /// </summary>
+        [Fact]
+        public void OnlyLowerCaseTest()
+        {
+            string once = laughter.LaughOnce();
+            AssertThat(once.ToCharArray()).AllSatisfy(c => char.IsLower(c));
+        }
+
+        /// <summary>
+        /// Checks that the laughter is repeated a correct amount of times.
+        /// </summary>
+        [Fact]
+        public void AmountTest()
+        {
+            string once = laughter.LaughOnce();
+            AssertThat(laughter.Laugh(2)).IsEqualTo($"{once}{once}");
+            AssertThat(laughter.Laugh(6)).IsEqualTo($"{once}{once}{once}{once}{once}{once}");
+        }
+
+        /// <summary>
+        /// Checks that the laughter is repeated a correct amount of times when capitalization is explicitly enabled.
+        /// </summary>
+        [Fact]
+        public void AmountCapitalizedTrueTest()
+        {
+            string once = laughter.LaughOnce();
+            string actual = laughter.Laugh(4, true);
+            AssertThat(actual.ToLowerInvariant()).IsEqualTo($"{once}{once}{once}{once}");
+            AssertThat(char.IsUpper(actual[0])).IsTrue();
+            AssertThat(actual.Substring(1).ToCharArray()).AllSatisfy(c => char.IsLower(c));
+        }
+
+        /// <summary>
+        /// Checks that the laughter is repeated a correct amount of times when capitalization is explicitly disabled.
+        /// </summary>
+        [Fact]
+        public void AmountCapitalizeFalseTest()
+        {
+            string once = laughter.LaughOnce();
+            string actual = laughter.Laugh(5, false);
+            AssertThat(actual.ToLowerInvariant()).IsEqualTo($"{once}{once}{once}{once}{once}");
+            AssertThat(actual.ToCharArray()).AllSatisfy(c => char.IsLower(c));
+        }
+
+        /// <summary>
+        /// Checks that the default laughter is repeated a correct amount of times.
+        /// </summary>
+        [Fact]
+        public void DefaultTest()
+        {
+            string once = laughter.LaughOnce();
+            AssertThat(laughter.Laugh()).IsEqualTo($"{once}{once}{once}");
+        }
+
+        /// <summary>
+        /// Checks that the default laughter is repeated a correct amount of times when capitalization is explicitly enabled.
+        /// </summary>
+        [Fact]
+        public void DefaultCapitalizedTrueTest()
+        {
+            string once = laughter.LaughOnce();
+            string actual = laughter.Laugh(true);
+            AssertThat(actual.ToLowerInvariant()).IsEqualTo($"{once}{once}{once}");
+            AssertThat(char.IsUpper(actual[0])).IsTrue();
+            AssertThat(actual.Substring(1).ToCharArray()).AllSatisfy(c => char.IsLower(c));
+        }
+
+        /// <summary>
+        /// Checks that the default laughter is repeated a correct amount of times when capitalization is explicitly disabled.
+        /// </summary>
+        [Fact]
+        public void DefaultCapitalizeFalseTest()
+        {
+            string once = laughter.LaughOnce();
+            string actual = laughter.Laugh(false);
+            AssertThat(actual.ToLowerInvariant()).IsEqualTo($"{once}{once}{once}");
+            AssertThat(actual.ToCharArray()).AllSatisfy(c => char.IsLower(c));
+        }
+    }
+}
---
+++ src/Nhahaha/Laughter.cs
@@ -0,0 +1,93 @@
+﻿using System.Text;
+
+namespace Nhahaha
+{
+    /// <summary>
+    /// Abstract class for producing laughter.
+    /// </summary>
+    public abstract class Laughter
+    {
+        private const int DefaultLaughterAmount = 3;
+
+        /// <summary>
+        /// Laughs the default amount of times.
+        /// </summary>
+        /// <returns>The produced localized laughter.</returns>
+        public string Laugh()
+            => Laugh(DefaultLaughterAmount);
+
+        /// <summary>
+        /// Laughs the default amount of times.
+        /// </summary>
+        /// <param name="capitalized">Determines whether or not the first letter should be capitalized.</param>
+        /// <returns>The produced localized laughter.</returns>
+        public string Laugh(bool capitalized)
+            => Laugh(DefaultLaughterAmount, capitalized);
+
+        /// <summary>
+        /// Laughs the given amount of times.
+        /// </summary>
+        /// <param name="times">The number of times to laugh.</param>
+        /// <returns>The produced localized laughter.</returns>
+        public string Laugh(int times)
+        {
+            StringBuilder sb = new StringBuilder();
+            string laugh = LaughOnce();
+            for (int i = 0; i < times; i++)
+            {
+                sb.Append(laugh);
+            }
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Laughs the given amount of times.
+        /// </summary>
+        /// <param name="times">The number of times to laugh.</param>
+        /// <param name="capitalized">Determines whether or not the first letter should be capitalized.</param>
+        /// <returns>The produced localized laughter.</returns>
+        public string Laugh(int times, bool capitalized)
+        {
+            string laughter = Laugh(times);
+
+            if (capitalized)
+            {
+                laughter = CapitalizeFirstLetter(laughter);
+            }
+
+            return laughter;
+        }
+
+        /// <summary>
+        /// Laughs once.
+        /// </summary>
+        /// <returns>The localized laughter.</returns>
+        public abstract string LaughOnce();
+
+        /// <summary>
+        /// Laughs once.
+        /// </summary>
+        /// <param name="capitalized">Determines whether or not the first letter should be capitalized.</param>
+        /// <returns>The localized laughter.</returns>
+        public string LaughOnce(bool capitalized)
+        {
+            string laughter = LaughOnce();
+
+            if (capitalized)
+            {
+                laughter = CapitalizeFirstLetter(laughter);
+            }
+
+            return laughter;
+        }
+
+        /// <summary>
+        /// Capitalizes the first letter of a string.
+        /// </summary>
+        /// <param name="text">String to capitalize the first letter of.</param>
+        /// <returns>The given string with the first letter capitalized.</returns>
+        private static string CapitalizeFirstLetter(string text)
+            => char.ToUpperInvariant(text[0]) + text.Substring(1);
+    }
+}
